940c634cf09f4d18d6bea76386a35081212fefec
phatdroid94 <phatdroid94@gmail.com>
2020-08-04 18:21:49 +0700

    time picker

--- app/src/main/java/com/example/timepickeranddatepickerdialog02062020/MainActivity.java
+++ app/src/main/java/com/example/timepickeranddatepickerdialog02062020/MainActivity.java
@@ -2,18 +2,22 @@ package com.example.timepickeranddatepickerdialog02062020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button mBtnTime,mBtnDate;
-    TextView mTvTime,mTvDate;
+    Button mBtnTime, mBtnDate;
+    TextView mTvTime, mTvDate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,10 +29,28 @@ public class MainActivity extends AppCompatActivity {
 
 //        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy");
 //        Log.d("BBB",simpleDateFormat.format(System.currentTimeMillis()) + "");
-        Calendar calendar = Calendar.getInstance();
-        int ngay = calendar.get(Calendar.DATE);
-        int thang = calendar.get(Calendar.MONTH);
-        int nam = calendar.get(Calendar.YEAR);
-        Log.d("BBB",nam + "");
+//        Calendar calendar = Calendar.getInstance();
+//        int ngay = calendar.get(Calendar.DATE);
+//        int thang = calendar.get(Calendar.MONTH);
+//        int nam = calendar.get(Calendar.YEAR);
+//        Log.d("BBB",nam + "");
+        mBtnTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog timePickerDialog = new TimePickerDialog(
+                                MainActivity.this,
+                                new TimePickerDialog.OnTimeSetListener() {
+                                    @Override
+                                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+                                    }
+                                },
+                                Calendar.getInstance().get(Calendar.HOUR_OF_DAY),
+                                Calendar.getInstance().get(Calendar.MINUTE),
+                                true);
+                timePickerDialog.show();
+
+            }
+        });
     }
 }
